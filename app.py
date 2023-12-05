758ffba13bc00c720ea54a6619959c213fe2ea45
Bomberman89 <kevinbucek@hotmail.com>
2023-12-05 21:34:23 +0000

    I reworked the layout of the html files so they would work more like templates. I created a main.html that contains placeholders/variables that the other html files will be inported into when the templates are rendered. I also started to figure out how I am going to show the blogs on the allBlogs page using the for loop.

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', title="Home Page")
+  return render_template('main.html', filename='home.html', title="Home Page")
 
 @app.route("/blogs")
 def blogposts():
-  return render_template('home.html', title="Blogs")
+  return render_template('main.html', filename='allBlogs.html', title="Blogs")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+
