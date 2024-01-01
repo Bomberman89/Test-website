6e78c2c6e42680a672425d818383032fb117d97f
Bomberman89 <kevinbucek@hotmail.com>
2024-01-01 23:19:20 +0000

    Created mock database to test iteration over data in a template. Created a blogitem template to include in the home template. Passed in the database information into the home page so the for loop could iterate over it. Created a requirements file to start preparing for a test deployment. Current requirements are just for the Flask and gunicorn. In app.py I am getting ready to try an API endpoint after I build out the site more.

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+BLOGS = [
+    {
+        'id' : 1,
+        'title' : 'Hello, world',
+        'content' : 'Lorem ipsum dolor sit amet',
+        'author' : 'John Doe'
+    },
+    {
+        'id' : 2,
+        'title' : 'Hello, country',
+        'content' : 'Lorem ipsum dolor sit amet',
+        'author' : 'John Doe'
+    },
+    {
+        'id' : 3,
+        'title' : 'Hello, state',
+        'content' : 'Lorem ipsum dolor sit amet',
+        'author' : 'John Doe'
+    }
+]
+
+
+def list_blogs():
+    return BLOGS
 
 
 @app.route("/")
 def hello_world():
-  return render_template('main.html', filename='home.html', title="Home Page")
+  return render_template('main.html', filename='home.html', title="Home Page", blogs=BLOGS)
 
 @app.route("/blogs")
 def blogposts():
